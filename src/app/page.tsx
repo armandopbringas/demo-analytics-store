0c4a641ba66b47ff0c10233ed8b87d2c87538cbf
Armando Bringas <bringas.armandop@gmail.com>
2026-03-12 01:00:41 -0600

    Fix hero image URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default async function HomePage({ searchParams }: { searchParams?: { page
       <section className="hero hero-full hero-full-bleed">
         <Image
           className="hero-image"
-          src="https://images.unsplash.com/photo-1674505613923-98085d3712fd?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="https://images.unsplash.com/photo-1674505613923-98085d3712fd?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%253D%253D"
           alt="Editorial moda"
           width={1200}
           height={680}
