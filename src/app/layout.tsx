40525b2fdb2d6ce188d7cd50edf35daf11140024
Armando Bringas <bringas.armandop@gmail.com>
2026-03-18 14:34:58 -0600

    chore: disable GA4 gtag implementation (migrate to GTM)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import Script from 'next/script';
 import { Providers } from './providers';
-import { GA_ID } from '@/lib/gtag';
 import { NavCartLink } from '@/components/nav-cart-link';
 import { FiShoppingBag } from 'react-icons/fi';
 import './globals.css';
@@ -28,19 +26,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </nav>
           <main className="container">{children}</main>
         </Providers>
-        {GA_ID ? (
-          <>
-            <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
-            <Script id="gtag-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', { send_page_view: true });
-              `}
-            </Script>
-          </>
-        ) : null}
       </body>
     </html>
   );
