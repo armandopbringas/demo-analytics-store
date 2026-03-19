0ddf63b5820f0a2700a56178128e224b8cffc4c8
Armando Bringas <bringas.armandop@gmail.com>
2026-03-18 18:55:56 -0600

    Add GTM snippet to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from "next/script";
 import Link from 'next/link';
 import { Providers } from './providers';
 import { NavCartLink } from '@/components/nav-cart-link';
@@ -13,7 +14,30 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+     {/* GTM: script (equivalente a <head>) */}
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MLJVGB9C');
+          `,
+        }}
+      />
       <body>
+        {/* GTM: noscript (primer hijo del body) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MLJVGB9C"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Providers>
           <nav className="nav">
             <Link href="/" className="nav-brand">
